bf8a79edc2f7e63ecf5380a89f18cb4eeac9fe96
김길수 <irebirth@naver.com>
2022-09-27 16:06:34 +0900

    feat: Issues 모델 추가

---
+++ Thingsflow-test/Model/Issues.swift
@@ -0,0 +1,13 @@
+//
+//  Issues.swift
+//  Thingsflow-test
+//
+//  Created by 김길수 on 2022/09/27.
+//
+
+import Foundation
+
+struct Issues: Decodable {
+    let number: Int?
+    let title: String?
+}
